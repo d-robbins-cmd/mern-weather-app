db1d028d744bcf57533594c99b4fc5993147a050
borgnine <derlrobbins@gmail.com>
2021-04-07 11:20:07 -0400

    adds path lib and conditional for production

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const mongoose = require('mongoose');
 
@@ -14,6 +15,17 @@ app.use(express.urlencoded({ extended: true }));
 // Use API routes from the api folder
 const apis = require("./api");
 app.use("/api", apis);
+
+// If in production, then use static frontend build files.
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, '../client/build')));
+
+    // Handle React routing, return all requests to React app
+    app.get('*', function(req, res) {
+        res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+    });
+}
 
 // Connect to Mongo
 mongoose.connect(process.env.DB, {
