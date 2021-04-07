a9c929688f37dc58c1af1dba0278b4edd5770a24
borgnine <derlrobbins@gmail.com>
2021-04-07 13:55:51 -0400

    proxy adjusted and back in

--- server/server.js
+++ server/server.js
@@ -16,6 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 const apis = require("./api");
 app.use("/api", apis);
 
+
 // If in production, then use static frontend build files.
 if (process.env.NODE_ENV === 'production') {
     // Serve any static files
